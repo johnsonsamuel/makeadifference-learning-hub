acaa43bf21cdd15f6ceb07744294b936280a98a3
Johnson Samuel <yoe.johnson@gmail.com>
2026-01-11 19:19:21 -0600

    Ignore .docusaurus folder

--- .docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("/Users/jenijohn/Documents/makeadifference-hub/makeadifference-learning-hub/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
-  require("/Users/jenijohn/Documents/makeadifference-hub/makeadifference-learning-hub/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/jenijohn/Documents/makeadifference-hub/makeadifference-learning-hub/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/jenijohn/Documents/makeadifference-hub/makeadifference-learning-hub/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/jenijohn/Documents/makeadifference-hub/makeadifference-learning-hub/src/css/custom.css"),
-];
